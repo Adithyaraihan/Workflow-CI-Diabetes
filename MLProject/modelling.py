7cebe4d6850038e63988103928d2f56656540e51
Adithya Raihan Pratama <144696680+Adithyaraihan@users.noreply.github.com>
2026-06-06 07:36:03 +0700

    Mengubah versi py dan mlflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,27 +6,21 @@ import mlflow
 import mlflow.sklearn
 
 def main():
-    
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    data_path = os.path.join(BASE_DIR, "diabetes_clean_manual.csv")
+    data_path = os.path.join(BASE_DIR,  "diabetes_clean_manual.csv")
     
     print(f"[INFO] Membaca dataset lokal dari: {data_path}")
-    if not os.path.exists(data_path):
-        raise FileNotFoundError(f"[ERROR] Dataset tidak ditemukan di path: {data_path}")
-        
     df = pd.read_csv(data_path)
     
-    #plit Fitur dan Target
     X = df.drop(columns=['Outcome'])
     y = df['Outcome']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    with mlflow.start_run(run_name="CI_Retrain_Local_Dataset"):
+    with mlflow.start_run():
         model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
         model.fit(X_train, y_train)
-        
         mlflow.sklearn.log_model(model, "model")
-        print("[INFO] CI Retraining dengan dataset lokal sukses dijalankan!")
+        print("[INFO] CI Retraining sukses dijalankan!")
 
 if __name__ == "__main__":
     main()
